78e16b53eac906642050fd300235c1c1b75e0b25
bobby-glitvhy <sebi_c@icloud.com>
2026-02-25 08:43:42 +0000

    Update MyPlugin.java

--- src/main/java/com/example/myplugin/MyPlugin.java
+++ src/main/java/com/example/myplugin/MyPlugin.java
@@ -1,26 +1,54 @@
 package com.example.myplugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MyPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getLogger().info("MyPlugin enabled!");
+        getLogger().info("NameChanger enabled!");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("MyPlugin disabled!");
+        getLogger().info("NameChanger disabled!");
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        if (command.getName().equalsIgnoreCase("hello")) {
-            sender.sendMessage("Hello from MyPlugin!");
+        if (command.getName().equalsIgnoreCase("setname")) {
+
+            if (!sender.hasPermission("namechanger.setname")) {
+                sender.sendMessage(ChatColor.RED + "You don't have permission!");
+                return true;
+            }
+
+            if (args.length < 2) {
+                sender.sendMessage(ChatColor.RED + "Usage: /setname <player> <newname>");
+                return true;
+            }
+
+            Player target = Bukkit.getPlayer(args[0]);
+
+            if (target == null) {
+                sender.sendMessage(ChatColor.RED + "Player not found!");
+                return true;
+            }
+
+            String newName = ChatColor.translateAlternateColorCodes('&', args[1]);
+            
+            target.setDisplayName(newName);
+            target.setPlayerListName(newName);
+
+            sender.sendMessage(ChatColor.GREEN + "Changed " + target.getName() + "'s name to " + newName);
+            target.sendMessage(ChatColor.YELLOW + "Your display name has been changed to " + newName);
+
             return true;
         }
 
